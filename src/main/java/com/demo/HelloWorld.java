005bc2452ea825741318576c6d9d37716871b26a
chengsiqi <csq_0512@163.com>
2019-07-30 17:38:29 +0800

    Initial commit

---
+++ src/main/java/com/demo/HelloWorld.java
@@ -0,0 +1,34 @@
+package com.demo;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author 程思琦
+ * @date 2019/7/30 10:56
+ * @description
+ **/
+@WebServlet("/myController")
+public class HelloWorld extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        System.err.println("---");
+        //解决乱码
+        req.setCharacterEncoding("UTF-8");
+        String name=req.getParameter("name");
+        req.setAttribute("name",name);
+        System.out.println(name);
+        req.getRequestDispatcher("index.jsp").forward(req, resp);
+    }
+
+}
